0828195a0232a4e752781a4b78c6ffc921b3e909
Nils Hott <mail@nilshott.com>
2022-01-23 12:02:42 +0100

    Clarified text of "Convert" button. And finally did some code formatting. Had to.

--- AppController.m
+++ AppController.m
@@ -13,72 +13,47 @@
 
 @synthesize pathAsString;
 
-- (IBAction)executeConversion:(id)sender
-{
+- (IBAction)executeConversion:(id)sender {
 	
-//	NSLog(@"Converting %@",pathAsString);
-	
+	//	NSLog(@"Converting %@",pathAsString);
 	
 	CSVParser *parser = [CSVParser new];
 	[parser openFileWithPath:pathAsString];
 	NSMutableArray *csvContent = [parser parseFile];
 	[parser closeFile];
 	
-	if (pathAsString != nil)
-	{
+	if (pathAsString != nil) {
 		
 		NSArray *keyArray = [csvContent objectAtIndex:0];
-		
 		NSMutableArray *plistOutputArray = [NSMutableArray array];
-		
 		NSInteger i = 0;
 		
-		for (NSArray *array in csvContent)
-		{
-			
-			
-			
+		for (NSArray *array in csvContent) {
 			NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
-			
 			NSInteger keyNumber = 0;
 			
-			for (NSString *string in array)
-			{
-				
+			for (NSString *string in array) {
 				[dictionary setObject:string forKey:[keyArray objectAtIndex:keyNumber]];
-				
 				keyNumber++;
-				
 			}
 			
-			if (i > 0)
-			{
+			if (i > 0) {
 				[plistOutputArray addObject:dictionary];
 			}
 			
 			i++;
-			
 		}
 		
-//		NSLog(@"Plist output array %@", plistOutputArray);
+		//		NSLog(@"Plist output array %@", plistOutputArray);
 		
 		NSMutableString *mutableString = [NSMutableString stringWithString:pathAsString];
 		[mutableString replaceOccurrencesOfString:@".csv" withString:@".plist" options:0 range:NSMakeRange([mutableString length]-4, 4)];
 		
 		NSURL *url = [NSURL fileURLWithPath:mutableString];
 		
-	
-		
-//		NSLog(@"Write to URL %@",url);
-		
+		//		NSLog(@"Write to URL %@",url);
 		[plistOutputArray writeToURL:url atomically:YES];
-		
 	}
-	
-	
-	
 }
 
-
 @end
-
